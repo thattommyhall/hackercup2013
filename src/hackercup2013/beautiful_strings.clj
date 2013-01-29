38500dc81dc249541f2c634ad9d41b834175971d
thattommyhall <thattommyhall@gmail.com>
2013-01-29 11:57:59 +0000

    only used vals, so sort them not the whole hash

--- src/hackercup2013/beautiful_strings.clj
+++ src/hackercup2013/beautiful_strings.clj
@@ -8,15 +8,9 @@
 (defn counts [s]
   (frequencies (filter letters (str/lower-case s))))
 
-(defn sorted-by-val [m]
-  (into (sorted-map-by (fn [key1 key2]
-                         (compare [(get m key2) key2]
-                                  [(get m key1) key1])))
-         m)
-)
 (defn beauty [s]
   (reduce + (map *
-                 (vals (sorted-by-val (counts s)))
+                 (reverse (sort (vals (counts s))))
                  (iterate dec 26))))
 
 (defn -main []
