431d4383e6e363c1bd2ca180b3c06101f35f49c9
thattommyhall <thattommyhall@gmail.com>
2013-01-29 11:43:23 +0000

    stray close paren

--- src/hackercup2013/beautiful_strings.clj
+++ src/hackercup2013/beautiful_strings.clj
@@ -12,8 +12,8 @@
   (into (sorted-map-by (fn [key1 key2]
                          (compare [(get m key2) key2]
                                   [(get m key1) key1])))
-         m)
-)
+         m))
+
 (defn beauty [s]
   (reduce + (map *
                  (vals (sorted-by-val (counts s)))
